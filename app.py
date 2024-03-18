d1695087474e69d888ced18b9b9555e548d19b4f
luanamsilva <luanamoura2002@yahoo.com.br>
2024-03-18 10:26:21 -0300

    models-SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask import Flask, request, jsonify
+from database import db
+from models.product import Product
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db.init_app(app)
 
 @app.route('/' , methods = ['GET'])
 def initial():
