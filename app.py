bc674e53761a44b9b7026a17969b15a03c0729cf
luanamsilva <luanamoura2002@yahoo.com.br>
2024-03-19 07:26:03 -0300

    add-product

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db.init_app(app)
 
+@app.route('/api/products/add', methods=['POST'])
+def add_product():
+   data = request.json
+   if "name" in data and "price" in data:
+     product = Product(name=data['name'], price= data['price'], description= data.get("description", ""))
+     db.session.add(product)
+     db.session.commit()
+     return jsonify({"message": "Produto cadastrado com sucesso!"})
+   return jsonify({"message": "Dados inválidos."}),400
 @app.route('/' , methods = ['GET'])
 def initial():
     return 'rota inicial'
