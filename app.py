f8decfbce453f65c4cf41535eeee2c5b6d361d8c
luanamsilva <luanamoura2002@yahoo.com.br>
2024-03-21 08:22:09 -0300

    get_product

--- app.py
+++ app.py
@@ -27,9 +27,16 @@ def delete_product(product_id):
       return jsonify({"message": "Produto deletado com sucesso"})
    return jsonify({"message":"Produto não encontrado!"}),404
 
-@app.route('/' , methods = ['GET'])
-def initial():
-    return 'rota inicial'
+@app.route('/api/products/<int:product_id>', methods=['GET'])
+def get_product(product_id):
+   product = Product.query.get(product_id)
+   if product:
+      return jsonify({"id": product_id,
+                      "name": product.name,
+                      "price": product.price,
+                      "description": product.description
+                      })
+   return jsonify({"message":"Produto não encontrado"}),404
 
 if __name__ == '__main__':
   app.run(debug=True)
