bdd51b296e1e627ed0e1c0b658966d4566af7903
luanamsilva <luanamoura2002@yahoo.com.br>
2024-03-25 08:39:54 -0300

    route-update

--- app.py
+++ app.py
@@ -38,5 +38,21 @@ def get_product(product_id):
                       })
    return jsonify({"message":"Produto não encontrado"}),404
 
+@app.route('/api/products/update/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+   product = Product.query.get(product_id)
+   if not product:
+      return jsonify({"message" : "Produto não encontrado"}), 404
+   
+   data = request.json
+   if "name" in data:
+      product.name = data['name']
+   if "price" in data:
+      product.price = data['price']
+   if "description" in data:
+      product.description = data['description']
+   db.session.commit()
+   return jsonify({"message": "Produto atualizado com sucesso!"})
+
 if __name__ == '__main__':
   app.run(debug=True)
